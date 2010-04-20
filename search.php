66488eeaff90ae719f116c8ec1c451dd2f7d31ce
unknown <Josh@.(none)>
2010-04-20 15:05:42 -0400

    Updated search

--- search.php
+++ search.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>toDo</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<script type="text/javascript" src="calendarDateInput.js"></script>
+
+<body>
+<div id="wrap">
+
+    
+	<div id="main">
 <?php
 
 $couch_dsn = "http://localhost:5984/";
@@ -20,5 +35,9 @@ $doc = $client->getDoc($_POST['searchid']);
     $title = $doc->title;
     echo "<tr><td  ><a href='entry_display.php?title=$title'>$title</a></td><td  >$body</td></tr>\n";
 
-exit;
 ?>
+  </table>
+   </div>
+</div>
+</body>
+</html>
