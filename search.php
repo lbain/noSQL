bfec92af73c35fdb128a6ada8c18642d1623176a
unknown <Josh@.(none)>
2010-04-22 11:57:04 -0400

    Updated search

--- search.php
+++ search.php
@@ -11,6 +11,15 @@
 <body>
 <div id="wrap">
 
+<table border="0" cellpadding="0" cellspacing="0" width="600">
+<tr><td align="right">
+<form action="search.php" method="post">
+<input type="text" name="searchid" value="" style="color: #000000; background: #FFFFFF">
+<input type="submit" value="Search">
+</form>
+</td></tr>
+</table>
+
     
 	<div id="main">
 <?php
@@ -30,10 +39,29 @@ if (!isset($_POST['searchid'])) {
 	exit;
 }
 
-$doc = $client->getDoc($_POST['searchid']);
-    $body =  $doc->body;
-    $title = $doc->title;
-    echo "<tr><td  ><a href='entry_display.php?title=$title'>$title</a></td><td  >$body</td></tr>\n";
+$all_docs = $client->getAllDocs();
+echo "<table id=\"hor-minimalist-b\">\n<tr><th>Title</th><th>Body</th><tr>\n\n";
+
+$foundsearch = 0;
+
+foreach ( $all_docs->rows as $row ) {
+
+	$doc = $client->getDoc($row->id);
+
+	if ($doc->title == $_POST['searchid']) {
+
+		$foundsearch = 1;
+
+		$body =  $doc->body;
+		$title = $doc->title;
+		echo "<tr><td  ><a href='entry_display.php?title=$title'>$title</a></td><td  >$body</td></tr>\n";
+
+	}
+ }
+
+if ($foundsearch == 0) {
+	echo "<tr><td colspan=\"2\">No Results Found</td></tr>\n";
+}
 
 ?>
   </table>
